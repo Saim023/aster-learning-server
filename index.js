107775c48d516c1a2a31827f45f55e4661fd77fa
saim-hossain <saimhossain023@gmail.com>
2022-10-26 14:46:10 +0600

    course details server updated

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const details = require('./data/details.json');
+const course = require('./data/course.json');
 
 app.get('/all-courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses', (req, res) => {
+    res.send(details);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const item = course.find(itm => itm.id == id);
+    res.send(item);
 });
 
 app.listen(port, () => {
